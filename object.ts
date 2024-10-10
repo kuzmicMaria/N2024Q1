063e8f10924f88899ae3786cca514ce28dd03e4c
Maria <Maria>
2024-10-10 17:08:07 +0300

    add:file object.ts

---
+++ object.ts
@@ -0,0 +1,14 @@
+const machineCHARACTERISTICS = {
+    salon: 'тёмный',
+    cushions: 'Передние' ,
+    headlights: 'Противотуманные' ,
+    heating: 'Сидений' ,
+    exterior: 'Легкосплавные диски' ,
+    comfort: 'Круиз-контроль' ,
+    multimedia: 'Мультимедийный экран' ,
+    securitySystems: 'Сигнализация' ,
+}
+
+
+
+console.log(machineCHARACTERISTICS)
